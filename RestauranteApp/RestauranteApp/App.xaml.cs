2d55e121d72fccd1e57030308165f36eb4efc25a
PaulaAriasArias <paulandrearias17@gmail.com>
2020-05-30 11:38:28 -0500

    ok

--- RestauranteApp/RestauranteApp/App.xaml.cs
+++ RestauranteApp/RestauranteApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using RestauranteApp.Views;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using RestauranteApp.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +19,9 @@ namespace RestauranteApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("1c44ddf2-0c70-40be-92dc-d922aba580f5",
+                   typeof(Analytics), typeof(Crashes));
+            
         }
 
         protected override void OnSleep()
